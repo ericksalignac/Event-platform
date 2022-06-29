c33bd3e87fece7b5ab744d6c8ebdee9142439841
ericksalignac <ericksalignac@gmail.com>
2022-06-29 10:24:05 -0300

    Aula 5 - instalação do codegen e deploy

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,14 +2,7 @@ import { gql, useMutation } from "@apollo/client";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
-
-const CREATE_SUBSCRIBER_MUTATION = gql`
-mutation CreateSubscriber ($name: String!, $email: String!) {
-  createSubscriber(data: {name: $name, email: $email}) {
-    id
-  }
-}
-`
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe () {
 const navigate = useNavigate();
@@ -17,7 +10,7 @@ const navigate = useNavigate();
 const [name, setName] = useState('');
 const [email, setEmail] = useState('');
 
-const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBER_MUTATION);
+const [createSubscriber, {loading}] = useCreateSubscriberMutation();
 
 async function handleScubscribe(event: FormEvent) {
   event.preventDefault();
